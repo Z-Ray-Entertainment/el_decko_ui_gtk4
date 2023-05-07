4a5e8c3782ed35bd590bc196ca96d438d38cbb4d
Vortex Acherontic <vortex@z-ray.de>
2023-05-07 18:24:49 +0200

    WIP setup UI

--- ed_ui_gtk4/__init__.py
+++ ed_ui_gtk4/__init__.py
@@ -1,10 +1,14 @@
 import sys
 
-from ed_ui_gtk4.el_decko import ElDecko
+from ed_ui_gtk4.el_decko_gtk4 import ElDecko
+from importlib.metadata import entry_points
 
-VERSION = "2022.12.28"
+VERSION = "2023.5.7"
 
 
 def run():
     app = ElDecko(application_id="de.zray.eldecko")
     app.run(sys.argv)
+    # core = entry_points(group='eldecko.core')
+    # run_core = core["start"].load()
+    # run_core()
--- ed_ui_gtk4/el_decko_gtk4.py
+++ ed_ui_gtk4/el_decko_gtk4.py
@@ -1,9 +1,10 @@
-import threading
 import gi
+import ed_core
 
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
 from gi.repository import Gtk, Adw
+from ed_core import streamdeck
 
 
 class MainWindow(Gtk.ApplicationWindow):
@@ -11,14 +12,32 @@ class MainWindow(Gtk.ApplicationWindow):
         super().__init__(*args, **kwargs)
         self.set_default_size(600, 250)
         self.set_title("El Decko")
-        self.box1 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        self.set_child(self.box1)
-        self.button = Gtk.Button(label="Hello")
-        self.button.connect("clicked", self.btn_hello)
-        self.box1.append(self.button)
+        self.header = Gtk.HeaderBar()
+        self.set_titlebar(self.header)
+        self.dd_decks = Gtk.DropDown()
+        self.header.pack_start(self.dd_decks)
 
-    def btn_hello(self, button):
-        print("Hello world!")
+        self.main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.set_child(self.main_box)
+        self.create_button_grid()
+
+    def create_button_grid(self):
+        if streamdeck.get_stream_decks():
+            for index, deck in enumerate(streamdeck.get_stream_decks()):
+                deck.open()
+                self.dd_decks.set_list_factory()
+                rows = deck.key_layout()[0]
+                columns = deck.key_layout()[1]
+                print("Rows: " + str(rows) + " | Columns: " + str(columns))
+                for i in range(0, rows):
+                    row_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+                    self.main_box.append(row_box)
+                    for j in range(0, columns):
+                        button = Gtk.Button(label=str(i) + " | " + str(j))
+                        row_box.append(button)
+                deck.close()
+        else:
+            print("No Elgato Stream Deck devices found!")
 
 
 class ElDecko(Adw.Application):
