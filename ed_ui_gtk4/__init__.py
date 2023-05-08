f77400d6682906ad68107cd45dec796dfe31f4b1
Vortex Acherontic <vortex@z-ray.de>
2023-05-08 16:50:55 +0200

    Bump version

--- ed_ui_gtk4/__init__.py
+++ ed_ui_gtk4/__init__.py
@@ -1,14 +1,10 @@
 import sys
 
 from ed_ui_gtk4.el_decko_gtk4 import ElDecko
-from importlib.metadata import entry_points
 
-VERSION = "2023.5.7"
+VERSION = "2023.5.8"
 
 
 def run():
     app = ElDecko(application_id="de.zray.eldecko")
     app.run(sys.argv)
-    # core = entry_points(group='eldecko.core')
-    # run_core = core["start"].load()
-    # run_core()
